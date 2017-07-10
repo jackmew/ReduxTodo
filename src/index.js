b8a8021b692c265b9706126eb5f25e79bfd32563
Jackmew <wonderfuljack123@gmail.com>
2017-07-10 13:24:30 +0800

    Adding a Todo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,134 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
+
+const todo = (state, action) => {
+   switch (action.type) {
+     case 'ADD_TODO':
+       return {
+         id: action.id,
+         text: action.text,
+         completed: false
+       };
+     case 'TOGGLE_TODO':
+       if (state.id !== action.id) {
+          return state;
+        }
+       return {
+         ...state,
+         completed: !state.completed
+       };  
+     default: 
+       return state;
+   }
+};
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        return [
+          ...state,
+          todo(undefined, action)
+        ];
+      case 'TOGGLE_TODO':
+        return state.map(t => todo(t, action));
+      default: 
+        return state;
+    }
+};
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default: 
+      return state;
+  }
+};
+
+const combineReducers = (reducers) => {
+  return (state = [], action) => {
+    const keys = Object.keys(reducers);
+
+    const reduced = keys.reduce((nextState, key) => {      
+      nextState[key] = reducers[key](state[key], action);
+      return nextState;
+    }, {});
+    return reduced;
+  };
+};
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+});
+
+
+// const { combineReducers } = Redux;
+// const todoApp = combineReducers({
+//   todos,
+//   visibilityFilter
+// });
+
+// const todoApp = (state = {}, action) => {
+//     return {
+//       todos: todos(
+//         state.todos,
+//         action
+//       ),
+//       visibilityFilter: visibilityFilter(
+//         state.visibilityFilter,
+//         action
+//       )
+//     };
+// }
+
+// const { createStore } = Redux;
+const store = createStore(todoApp);
+// view
+let nextTodoId = 0;
 class TodoApp extends Component {
     render() {
         return (
-            <h1>TodoApp Boilerplate</h1>
+            <div>
+                <input 
+                    ref={node => {
+                        this.input = node;
+                    }} 
+                />
+                <button
+                    onClick={() => {
+                        store.dispatch({
+                            type: 'ADD_TODO',
+                            text: this.input.value,
+                            id: nextTodoId++
+                        });
+                        this.input.value = '';
+                    }}
+                >
+                Add Todo
+                </button>
+                <ul>
+                    {this.props.todos.map(todo => {
+                        return (
+                            <li key={todo.id}>
+                                {todo.text}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
         );
     }
 }
 
-ReactDOM.render(<TodoApp />, document.querySelector('.container'));
+const render = () => {
+    console.log('render');
+    ReactDOM.render(<TodoApp todos={store.getState().todos} />, document.querySelector('.container'));
+};
+
+store.subscribe(render);
+render();
