e9f1eb93ec1a356f980b9d0c8c5ff9c5091d82e7
Jackmew <wonderfuljack123@gmail.com>
2017-07-10 18:12:59 +0800

    FilterLink connect to Link

--- src/index.js
+++ src/index.js
@@ -155,40 +155,6 @@ const Footer = () => {
         </p>
     );
 };
-class FilterLink extends Component {
-    componentDidMount() {
-        const { store } = this.context;
-        this.unsubscribe = store.subscribe(() => {
-            this.forceUpdate();
-        });
-    }
-    componentWillUnMount() {
-        this.unsubscribe();
-    }
-    render() {
-        const props = this.props;
-        const { store } = this.context;
-        const state = store.getState();
-        
-        return (
-            <Link 
-                active={props.filter === state.visibilityFilter}
-                onClick={() => {
-                    store.dispatch({
-                        type: 'SET_VISIBILITY_FILTER',
-                        filter: props.filter
-                    });                    
-                }}
-            >
-                {props.children}
-            </Link>
-        );
-    }
-}
-FilterLink.contextTypes = {
-    store: React.PropTypes.object
-};
-// link
 const Link = ({ active, children, onClick }) => {
     if (active) {
         return <span>{children}</span>;
@@ -204,6 +170,23 @@ const Link = ({ active, children, onClick }) => {
         </a>
     );
 };
+const mapStateToLinkProps = (state, ownProps) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
+    };
+};
+const mapDispatchToLinkProps = (dispatch, ownProps) => {
+    return {
+        onClick: () => {
+            dispatch({
+                type: 'SET_VISIBILITY_FILTER',
+                filter: ownProps.filter
+            });                    
+        }
+    };
+};
+const FilterLink = connect(mapStateToLinkProps, mapDispatchToLinkProps)(Link);
+
 const getVisibleTodos = (todos, filter) => {
     switch (filter) {
         case 'SHOW_ALL':
